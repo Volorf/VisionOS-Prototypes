a2cb7de74addb957f918ecc43b7119d33107f964
Oleg Frolov <frolololeg@gmail.com>
2024-01-05 23:10:41 +0000

    Initial Commit

---
+++ VisionOS Prototypes/VisionOS_PrototypesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOS_PrototypesApp.swift
+//  VisionOS Prototypes
+//
+//  Created by Oleg Frolov on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct VisionOS_PrototypesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
