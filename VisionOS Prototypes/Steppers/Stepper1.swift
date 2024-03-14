6fa10d2b086836a59f660043260ffd8768f64ccb
Oleg Frolov <frolololeg@gmail.com>
2024-03-14 22:17:00 +0000

    cleaned up

--- VisionOS Prototypes/Steppers/Stepper1.swift
+++ VisionOS Prototypes/Steppers/Stepper1.swift
@@ -19,17 +19,17 @@ struct Stepper1: View
     
     // Settings
     private let angleOffset: Double = 20.0
-    private let highOpacity: Double = 1.0
     private let lowOpacity: Double = 0.2
     private let buttonWidth: Double = 288.0
     private let buttonHeight: Double = 96.0
     private let sideTriggerArea: Double = 96.0
     private let anchorXOffset: Double = 48.0
+    private let popScale: Double = 1.5
     
     // Animation
     private let animDuration: Double = 0.5
 
-    // State
+    // States
     @State private var isPressed: Bool = false;
     @State private var currentAndleOffset: Double = 0.0
     @State private var pressedSide: Side = .Middle
@@ -37,7 +37,9 @@ struct Stepper1: View
     @State private var decimalCounter: Int = 0
     @State private var easing: Animation = .easeOut
     @State private var counterScale: Double = 1.0
-    
+    @State private var rotateIconPlus: Double = 0.0
+    @State private var rotateIconMinus: Double = 0.0
+    @State private var buttonScaler: Double = 1.0
     
     var body: some View
     {
@@ -45,71 +47,73 @@ struct Stepper1: View
         {
             HStack(spacing: 0)
             {
-
                 HStack(spacing: 0)
                 {
-                    Image("minus").opacity(decimalCounter <= 0 ? 0.2 : 1.0).foregroundStyle(.secondary)
+                    Image("minus")
+                        .opacity(decimalCounter <= 0 ? lowOpacity : 1.0)
+                        .foregroundStyle(.secondary)
+                        .scaleEffect(pressedSide == .Left ? counterScale : 1.0)
+                        .rotationEffect(.degrees(rotateIconMinus))
+                    
                     Spacer()
+                    
                     Text("\(String(decimalCounter, radix: 2))")
                         .animation(.none)
                         .frame(width: 128)
                         .foregroundStyle(.secondary)
-                        .scaleEffect(counterScale)
-                        
-                        
+
                     Spacer()
-                    Image("plus").foregroundStyle(.secondary)
+                    
+                    Image("plus")
+                        .foregroundStyle(.secondary)
+                        .scaleEffect(pressedSide == .Right ? counterScale : 1.0)
+                        .rotationEffect(.degrees(rotateIconPlus))
                 }
                 .font(.system(size: 32, design: .monospaced))
                 .fontWeight(.regular)
                 .padding(36.0)
-                
             }
-            
-            
         }
         .background(isPressed ? .white.opacity(0.1) : .clear)
         .glassBackgroundEffect()
-        
         .frame(width: buttonWidth, height: buttonHeight)
-        
-        
-        
-        
-        
         .rotation3DEffect(.degrees(currentAndleOffset), axis: (0.0, 1.0, 0.0), anchor: buttonAnchor)
-        
         .offset(z: anchorXOffset * 2)
+        .scaleEffect(buttonScaler)
         .gesture(DragGesture(minimumDistance: 0)
             .onChanged { clickData in
                 
                 if (!isPressed)
                 {
-                    
-                    
                     pressedSide = getSideThatWasPressed(clickData: clickData)
                     
                     let normXOffset = anchorXOffset / buttonWidth
                     
                     if (pressedSide == .Left)
                     {
+                        rotateIconMinus -= 180.0
                         currentAndleOffset = -angleOffset
                         buttonAnchor = UnitPoint3D(x: 1.0 - normXOffset, y: 0.5, z: normXOffset)
                     }
                     
                     if (pressedSide == .Right)
                     {
+                        rotateIconPlus += 180.0
                         currentAndleOffset = angleOffset
                         buttonAnchor = UnitPoint3D(x: normXOffset, y: 0.5, z: normXOffset)
                     }
                     
+                    if (pressedSide == .Middle)
+                    {
+                        buttonScaler = 0.9
+                    }
+                    
                     easing = Animation.snappy(duration: animDuration/2)
-                    
-                    counterScale = 1.2
-                    
-//                    print(pressedSide)
+                    counterScale = popScale
+
                     print("Down")
 //                    print(clickData)
+//                    print(pressedSide)
                     
                     isPressed = true
                     
@@ -140,26 +144,18 @@ struct Stepper1: View
                     }
                     
                     easing = Animation.spring(duration: animDuration * 2)
-                    
                     counterScale = 1.0
+                    buttonScaler = 1.0
                     
                     print("Up")
-                    
                 }
             })
-        
         .onAppear()
         {
             
         }
-        
         .animation(easing, value: currentAndleOffset)
         .animation(easing, value: counterScale)
-        
-        
-        
-        
-        
     }
     
     func getSideThatWasPressed(clickData: DragGesture.Value) -> Side
